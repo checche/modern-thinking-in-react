c164b856f3e07ea2ec153c0082464a6b69f3dc4f
TakahiroHirata <takakun47@gmail.com>
2022-08-19 16:47:25 +0900

    add FilterableProductTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './App.css';
 import {Product} from './entity/product';
-import {ProductTable} from './ui/ProductTable';
-import {SearchBar} from './ui/SearchBar';
-
-
-const FilterableProductTable = ({products}:{ products: Product[]}) => {
-  const [filterText, setFilterText] = useState('');
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  function handleFilterTextChange(filterText:string):void {
-    setFilterText(filterText);
-  }
-
-  function handleInStockChange(inStockOnly:boolean):void {
-    setInStockOnly(inStockOnly);
-  }
-
-  return (
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={handleFilterTextChange}
-        onInStockChange={handleInStockChange}
-      />
-      <ProductTable
-        products={products}
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-      />
-    </div>
-  );
-};
+import {FilterableProductTable} from './ui/FilterableProductTable';
 
 
 const PRODUCTS: Product[] = [
---
+++ src/ui/FilterableProductTable.tsx
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import {Product} from '../entity/product';
+import {ProductTable} from './ProductTable';
+import {SearchBar} from './SearchBar';
+
+export const FilterableProductTable = ({products}:{ products: Product[]}) => {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  function handleFilterTextChange(filterText:string):void {
+    setFilterText(filterText);
+  }
+
+  function handleInStockChange(inStockOnly:boolean):void {
+    setInStockOnly(inStockOnly);
+  }
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={handleFilterTextChange}
+        onInStockChange={handleInStockChange}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+};
